a1b5121bae8b0331ec200c832838a329c7edb1c4
HannesDahl <hannes.dahlvik@gmail.com>
2019-08-16 15:32:01 +0300

    added symbol input

--- Kalkylator/scipt.js
+++ Kalkylator/scipt.js
@@ -1,9 +1,8 @@
-// Numbers
 let num1 = document.getElementById("number1");
 let num2 = document.getElementById("number2");
 let answer = document.getElementById("answer");
+let symbol = document.getElementById("symbol");
 
-// MDAS
 let addition = document.getElementById("addition");
 let subtract = document.getElementById("subtract");
 let multiply = document.getElementById("multiply");
@@ -16,17 +15,21 @@ multiply.addEventListener("click", multiplyFunction);
 division.addEventListener("click", divisionFunction);
 
 function additionFunction() {
-    answer.value = parseFloat(num1.value) + parseFloat(num2.value)
+    answer.value = parseFloat(num1.value) + parseFloat(num2.value);
+    symbol.value = ("+");
 };
 
 function subtractFunction() {
-    answer.value = parseFloat(num1.value) - parseFloat(num2.value)
+    answer.value = parseFloat(num1.value) - parseFloat(num2.value);
+    symbol.value = ("-");
 };
 
 function multiplyFunction() {
-    answer.value = parseFloat(num1.value) * parseFloat(num2.value)
+    answer.value = parseFloat(num1.value) * parseFloat(num2.value);
+    symbol.value = ("*");
 };
 
 function divisionFunction() {
-    answer.value = parseFloat(num1.value) / parseFloat(num2.value)
+    answer.value = parseFloat(num1.value) / parseFloat(num2.value);
+    symbol.value = ("/");
 };
